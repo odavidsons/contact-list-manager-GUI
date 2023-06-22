335fca5a90d42218d22d71c82dca0a7dba0d64c3
David Santos <davidsantos2807@gmail.com>
2023-06-22 12:54:28 +0100

    GUI code restructure

--- contactListManager.py
+++ contactListManager.py
@@ -6,7 +6,8 @@ Made by David Santos - https://github.com/odavidsons/contact-list-manager-GUI
 """
 #File imports
 import dbconnection #File containing database class/functions
-import filehandling
+import filehandling #File containing config/log file handling functions
+import views #File containing GUI creation for windows inside the app
 #Module imports
 import tkinter as tk
 from tkinter import filedialog as fd
@@ -26,6 +27,7 @@ class App(tk.Frame):
     def __init__(self,master=None):
         tk.Frame.__init__(self,master)
         self.master.config(width=300,height=300)
+        self.views = views.views(self)
         #Main window UI widgets
         self.menubar = tk.Menu(self)
         self.menubar = tk.Menu(self)
@@ -33,11 +35,11 @@ class App(tk.Frame):
         self.contactsMenu.add_command(label="Import contacts",command=self.importContactsJSON,font=self.fontMedium)
         self.contactsMenu.add_command(label="Export contacts",command=self.exportContactsJSON,font=self.fontMedium)
         self.databaseMenu = tk.Menu(self.menubar, tearoff=0)
-        self.databaseMenu.add_command(label="Connect",command=self.connectDBWindow,font=self.fontMedium)
+        self.databaseMenu.add_command(label="Connect",command=self.views.connectDBWindow,font=self.fontMedium)
         self.databaseMenu.add_command(label="Disconnect",command=self.disconnectDB,state="disabled",font=self.fontMedium)
         self.menubar.add_cascade(label="Contacts", menu=self.contactsMenu,font=self.fontLarge)
         self.menubar.add_cascade(label="Database", menu=self.databaseMenu,font=self.fontLarge)
-        self.menubar.add_command(label="Settings",command=self.settingsWindow,font=self.fontLarge)
+        self.menubar.add_command(label="Settings",command=self.views.settingsWindow,font=self.fontLarge)
         self.menubar.add_command(label="Exit", command=self.quit,font=self.fontLarge)
         self.master.config(menu=self.menubar)
         self.master.title("Contact Manager")
@@ -46,15 +48,15 @@ class App(tk.Frame):
         self.master.grid_columnconfigure(0, weight=1)
         self.optionsFrame = tk.Frame(self.master,bg="#2b7287")
         self.optionsFrame.grid(row=0,column=0,sticky="nsew")
-        self.viewBtn = tk.Button(self.optionsFrame,text="View",bg="teal",fg="white",highlightthickness=0,command=self.viewContact,font=self.fontLarge)
+        self.viewBtn = tk.Button(self.optionsFrame,text="View",bg="teal",fg="white",highlightthickness=0,command=self.views.viewContact,font=self.fontLarge)
         self.viewBtn.grid(row=0,column=0,pady=20,padx=20)
-        self.addBtn = tk.Button(self.optionsFrame,text="Add",bg="teal",fg="white",highlightthickness=0,command=self.addContactWindow,font=self.fontLarge)
+        self.addBtn = tk.Button(self.optionsFrame,text="Add",bg="teal",fg="white",highlightthickness=0,command=self.views.addContactWindow,font=self.fontLarge)
         self.addBtn.grid(row=1,column=0,pady=20,padx=20)
-        self.editBtn = tk.Button(self.optionsFrame,text="Edit",bg="teal",fg="white",highlightthickness=0,command=self.editContactWindow,font=self.fontLarge)
+        self.editBtn = tk.Button(self.optionsFrame,text="Edit",bg="teal",fg="white",highlightthickness=0,command=self.views.editContactWindow,font=self.fontLarge)
         self.editBtn.grid(row=2,column=0,pady=20,padx=20)
         self.removeBtn = tk.Button(self.optionsFrame,text="Remove",bg="teal",fg="white",highlightthickness=0,command=self.removeContact,font=self.fontLarge)
         self.removeBtn.grid(row=3,column=0,pady=20,padx=20)
-        self.make_dynamic(self.optionsFrame)
+        self.views.make_dynamic(self.optionsFrame)
         self.contactsFrame = tk.Frame(self.master,bg="#b0b0b0")
         self.contactsFrame.grid(sticky="nsew",row=0,column=1)
         self.contactList = tk.Listbox(self.contactsFrame,width=50,justify="center",font=self.fontMedium)
@@ -63,7 +65,7 @@ class App(tk.Frame):
         self.scrollbar_y.grid(row=0,column=1,sticky="ns")
         self.contactList.config(yscrollcommand=self.scrollbar_y.set)
         self.scrollbar_y.config(command = self.contactList.yview)
-        self.make_dynamic(self.contactsFrame)
+        self.views.make_dynamic(self.contactsFrame)
         self.statusFrame = tk.Frame(self.master)
         self.statusFrame.grid(sticky="ew",row=1,column=0,columnspan=2)
         self.counterLabel = tk.Label(self.statusFrame,text="Total contacts: 0",bg="#2b7287",font=self.fontMedium)
@@ -110,38 +112,6 @@ class App(tk.Frame):
             else: msg.showerror(title="Failed",message="You have no data to export!")
         except: msg.showerror(title="Error",message="There was an error while exporting your contacts")
 
-    #Create a window with a contact form
-    def addContactWindow(self):
-        window = tk.Toplevel(self)
-        window.title("Add a contact")
-        window.geometry(f'+{self.master.winfo_rootx()}+{self.master.winfo_rooty()}')
-        body = tk.Frame(window)
-        body.grid()
-        name = tk.Label(body,text="Name:",font=self.fontMedium)
-        name.grid(row=0,column=0)
-        inputName = tk.Entry(body,font=self.fontMedium)
-        inputName.grid(row=0,column=1,pady=5,padx=15)
-        phone = tk.Label(body,text="Phone number:",font=self.fontMedium)
-        phone.grid(row=1,column=0)
-        inputPhone = tk.Entry(body,font=self.fontMedium)
-        inputPhone.grid(row=1,column=1,pady=5,padx=15)
-        email = tk.Label(body,text="Email:",font=self.fontMedium)
-        email.grid(row=2,column=0)
-        inputEmail = tk.Entry(body,font=self.fontMedium)
-        inputEmail.grid(row=2,column=1,pady=5,padx=15)
-        address = tk.Label(body,text="Address:",font=self.fontMedium)
-        address.grid(row=3,column=0)
-        inputAddress = tk.Entry(body,font=self.fontMedium)
-        inputAddress.grid(row=3,column=1,pady=5,padx=15)
-        gender = tk.Label(body,text="Gender:",font=self.fontMedium)
-        gender.grid(row=4,column=0)
-        gender_value = tk.StringVar(body,"Select a gender")
-        inputGender = tk.OptionMenu(body,gender_value,"Male","Female","Other")
-        inputGender.grid(row=4,column=1,pady=5,padx=15)
-        addBtn = tk.Button(body,text="OK",command=lambda: [self.addContact(window,inputName.get(),inputPhone.get(),inputEmail.get(),inputAddress.get(),gender_value.get())])
-        addBtn.grid(columnspan=2,pady=5,padx=15)
-        self.make_dynamic(window)
-
     #Execute the operations for adding a new contact
     def addContact(self,window,name,phone,email,address,gender):
         if name != "":
@@ -155,120 +125,6 @@ class App(tk.Frame):
                 self.contactList.insert(tk.END,name)
             window.destroy()
         else: msg.showwarning(title="Missing field",message="Please enter at least the contact name")
-
-    #Create a window with the contact details
-    def viewContact(self):
-        try:
-            selected = self.contactList.get(self.contactList.curselection())
-            #If database connection is set
-            if self.databaseStatus == True:
-                contact = self.db.getContactByName(selected)
-                view_name = contact[0]["name"]
-                view_phone = contact[0]["phone_number"]
-                view_email = contact[0]["email"]
-                view_address = contact[0]["address"]
-                view_gender = contact[0]["gender"]
-            else:
-                #For local data
-                for contact in self.contactsDataJSON:
-                    if contact["name"] == selected:
-                        view_name = contact["name"]
-                        view_phone = contact["phone_number"]
-                        view_email = contact["email"]
-                        view_address = contact["address"]
-                        view_gender = contact["gender"]
-            #Open view window
-            window = tk.Toplevel(self)
-            window.title("View contact")
-            window.geometry(f'+{self.master.winfo_rootx()}+{self.master.winfo_rooty()}')
-            body = tk.Frame(window)
-            body.grid()
-            name = tk.Label(body,text="Name:",font=self.fontMedium)
-            name.grid(row=0,column=0,pady=5,padx=15)
-            inputName = tk.Label(body,text=view_name,font=self.fontMedium)
-            inputName.grid(row=0,column=1)
-            phone = tk.Label(body,text="Phone number:",font=self.fontMedium)
-            phone.grid(row=1,column=0,pady=5,padx=15)
-            inputPhone = tk.Label(body,text=view_phone,font=self.fontMedium)
-            inputPhone.grid(row=1,column=1)
-            email = tk.Label(body,text="Email:",font=self.fontMedium)
-            email.grid(row=2,column=0,pady=5,padx=15)
-            inputEmail = tk.Label(body,text=view_email,font=self.fontMedium)
-            inputEmail.grid(row=2,column=1)
-            address = tk.Label(body,text="Address:",font=self.fontMedium)
-            address.grid(row=3,column=0,pady=5,padx=15)
-            inputAddress = tk.Label(body,text=view_address,font=self.fontMedium)
-            inputAddress.grid(row=3,column=1)
-            gender = tk.Label(body,text="Gender:",font=self.fontMedium)
-            gender.grid(row=4,column=0,pady=5,padx=15)
-            inputGender = tk.Label(body,text=view_gender,font=self.fontMedium)
-            inputGender.grid(row=4,column=1)
-            addBtn = tk.Button(body,text="Close",font=self.fontMedium,command=window.destroy)
-            addBtn.grid(columnspan=2,pady=5,padx=15)
-            self.make_dynamic(window)
-        except: pass
-            
-    #Create a window with the contact edit form
-    def editContactWindow(self):
-        try:
-            selected = self.contactList.get(self.contactList.curselection())
-            selected_index = self.contactList.curselection()
-            #If database connection is set
-            if self.databaseStatus == True:
-                contact = self.db.getContactByName(selected)
-                edit_name = contact[0]["name"]
-                edit_phone = contact[0]["phone_number"]
-                edit_email = contact[0]["email"]
-                edit_address = contact[0]["address"]
-                edit_gender = contact[0]["gender"]
-            else:
-                #For local data
-                for contact in self.contactsDataJSON:
-                    if contact["name"] == selected:
-                        edit_name = contact["name"]
-                        edit_phone = contact["phone_number"]
-                        edit_email = contact["email"]
-                        edit_address = contact["address"]
-                        edit_gender = contact["gender"]
-            #Open edit window
-            window = tk.Toplevel(self)
-            window.title("Edit contact")
-            window.geometry(f'+{self.master.winfo_rootx()}+{self.master.winfo_rooty()}')
-            body = tk.Frame(window)
-            body.grid()
-            name = tk.Label(body,text="Name:",font=self.fontMedium)
-            name.grid(row=0,column=0)
-            inputName = tk.Entry(body,text=edit_name,font=self.fontMedium)
-            inputName.grid(row=0,column=1,pady=5,padx=15)
-            inputName.delete(0,tk.END)
-            inputName.insert(tk.END,edit_name)
-            phone = tk.Label(body,text="Phone number:",font=self.fontMedium)
-            phone.grid(row=1,column=0)
-            inputPhone = tk.Entry(body,text=edit_phone,font=self.fontMedium)
-            inputPhone.grid(row=1,column=1,pady=5,padx=15)
-            inputPhone.delete(0,tk.END)
-            inputPhone.insert(tk.END,edit_phone)
-            email = tk.Label(body,text="Email:",font=self.fontMedium)
-            email.grid(row=2,column=0)
-            inputEmail = tk.Entry(body,text=edit_email,font=self.fontMedium)
-            inputEmail.grid(row=2,column=1,pady=5,padx=15)
-            inputEmail.delete(0,tk.END)
-            inputEmail.insert(tk.END,edit_email)
-            address = tk.Label(body,text="Address:",font=self.fontMedium)
-            address.grid(row=3,column=0)
-            inputAddress = tk.Entry(body,text=edit_address,font=self.fontMedium)
-            inputAddress.grid(row=3,column=1,pady=5,padx=15)
-            inputAddress.delete(0,tk.END)
-            inputAddress.insert(tk.END,edit_address)
-            gender = tk.Label(body,text="Gender:",font=self.fontMedium)
-            gender.grid(row=4,column=0)
-            gender_value = tk.StringVar(body,edit_gender)
-            inputGender = tk.OptionMenu(body,gender_value,"Male","Female","Other")
-            inputGender.grid(row=4,column=1,pady=5,padx=15)
-            addBtn = tk.Button(body,text="OK",command=lambda: [self.editContact(window,selected_index,inputName.get(),inputPhone.get(),inputEmail.get(),inputAddress.get(),gender_value.get())])
-            addBtn.grid(columnspan=2,pady=5,padx=15)
-            self.make_dynamic(window)
-        except: pass
 
     #Execute the operations for editing a contact
     def editContact(self,window,selected_index,name,phone,email,address,gender):
@@ -310,90 +166,11 @@ class App(tk.Frame):
                     self.contactList.delete(selected_index,selected_index)
         except: pass
 
-    #Create a Window for inputing the database details
-    def connectDBWindow(self):
-        database_details = self.filehandling.loadDatabaseConfig() #Load the server details from the configuration file
-        window = tk.Toplevel(self)
-        window.title("Database details")
-        window.geometry(f'+{self.master.winfo_rootx()}+{self.master.winfo_rooty()}')
-        body = tk.Frame(window,padx=20,pady=20)
-        body.grid()
-        DBhost = tk.Label(body,text="Host:",font=self.fontMedium)
-        DBhost.grid(row=0,column=0)
-        inputDBhost = tk.Entry(body,font=self.fontMedium)
-        inputDBhost.grid(row=0,column=1,pady=5,padx=15)
-        inputDBhost.insert(0,database_details[0])
-        DBuser = tk.Label(body,text="User:",font=self.fontMedium)
-        DBuser.grid(row=1,column=0)
-        inputDBuser = tk.Entry(body,font=self.fontMedium)
-        inputDBuser.grid(row=1,column=1,pady=5,padx=15)
-        inputDBuser.insert(0,database_details[1])
-        DBpassword = tk.Label(body,text="Password:",font=self.fontMedium)
-        DBpassword.grid(row=2,column=0)
-        inputDBpassword = tk.Entry(body,font=self.fontMedium)
-        inputDBpassword.grid(row=2,column=1,pady=5,padx=15)
-        inputDBpassword.insert(0,database_details[2])
-        DBname = tk.Label(body,text="Database name:",font=self.fontMedium)
-        DBname.grid(row=3,column=0)
-        inputDBname = tk.Entry(body,font=self.fontMedium)
-        inputDBname.grid(row=3,column=1,pady=5,padx=15)
-        inputDBname.insert(0,database_details[3])
-        addBtn = tk.Button(body,text="OK",font=self.fontMedium,command=lambda: [self.connectDB(window,inputDBhost.get(),inputDBuser.get(),inputDBpassword.get(),inputDBname.get())])
-        addBtn.grid(columnspan=2,pady=5,padx=15)
-        self.make_dynamic(window)
-
     #Update the contact list counter
     def contactCounter(self):
         n_contacts = self.contactList.size()
         self.counterLabel.config(text=f"Total contacts: {n_contacts}")
         self.after(1000,self.contactCounter)
-
-    #Create a window for changing the application settings
-    def settingsWindow(self):
-        global_settings = self.filehandling.loadGlobalConfig()
-        autoConnect = tk.StringVar()
-        autoConnect.set(global_settings[0])
-        keepLogs = tk.StringVar()
-        keepLogs.set(global_settings[1])
-        
-        window = tk.Toplevel(self)
-        window.title("Settings")
-        window.geometry(f'+{self.master.winfo_rootx()}+{self.master.winfo_rooty()}')
-        body = tk.Frame(window, padx=20,pady=20)
-        body.grid()
-        label1 = tk.Label(window,text="Auto connect on open:",pady=15,padx=15,font=self.fontMedium)
-        label1.grid(row=0,column=0)
-        chk_autoConn = tk.Checkbutton(window,variable=autoConnect,onvalue=1,offvalue=0)
-        chk_autoConn.grid(row=0,column=1)
-        label2 = tk.Label(window, text="Keep logs:",pady=15,padx=15,font=self.fontMedium)
-        label2.grid(row=1,column=0)
-        chk_keepLogs = tk.Checkbutton(window,variable=keepLogs,onvalue=1,offvalue=0)
-        chk_keepLogs.grid(row=1,column=1)
-        label3 = tk.Label(window,text="Delete saved connection details",font=self.fontMedium)
-        label3.grid(row=2,column=0)
-        delDBBtn = tk.Button(window,text="Delete",font=self.fontMedium, command=self.filehandling.resetDatabaseConfig)
-        delDBBtn.grid(row=2,column=1,pady=15,padx=15)
-        database_details = self.filehandling.loadDatabaseConfig()
-        if database_details[0] == '': delDBBtn.config(state="disabled")
-        saveBtn = tk.Button(window, text="Save",font=self.fontMedium, command=lambda: self.filehandling.saveGlobalConfig(window,autoConnect.get(),keepLogs.get()))
-        saveBtn.grid(row=3,column=0,columnspan=2,pady=15,padx=15)
-        self.make_dynamic(window)
-
-    #Make a window's widgets dynamic when resizing
-    def make_dynamic(self,window):
-        col_count,row_count = window.grid_size()
-        
-        for i in range(row_count):
-            window.grid_rowconfigure(i, weight=1)
-
-        for i in range(col_count):
-            window.grid_columnconfigure(i, weight=1)
-
-        for child in window.children.values():
-            try:
-                child.grid_configure(sticky="nsew")
-                self.make_dynamic(child)
-            except: pass
 
     """ *---------------------------------------------------------------*
         |         Functions for config and logs file handling           |
